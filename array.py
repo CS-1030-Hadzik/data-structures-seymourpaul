b90f5f8df67a365fdc65adb0da068bf797a4c428
seymourpaul <59572718+seymourpaul@users.noreply.github.com>
2020-04-24 12:07:07 -0600

    Add files via upload

---
+++ array.py
@@ -0,0 +1,40 @@
+print ("Seymour Paul")
+
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+print (cars)
+
+x = len(cars)
+
+print (x)
+
+cars.append('Buick')
+
+print (cars)
+
+print (cars[3])
+
+cars.insert(2, 'Toyota')
+
+print (cars)
+
+cars.pop(4)
+
+print (cars)
+
+cars.sort()
+
+print (cars)
+
+cars.sort(reverse=True)
+
+print (cars)
+
+my_array_length = 7
+
+array_string = 'The length of my array is'
+
+print (array_string, my_array_length)
+
+
+
